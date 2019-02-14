d96325057cfa98e8578945b44638484abeaeda3f
mathew1122 <mandork57@gmail.com>
2019-02-14 13:43:41 -0800

    Game and exe upload
    Uploaded the game and executable files.

---
+++ Jewel Mining Game/Assets/Scripts/SpawnJewels.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnJewels : MonoBehaviour 
+{
+	public int xRange = 10;
+	public int yRange = 3;
+	public int numObjects = 16;
+
+	public GameObject[] objects;
+
+
+	void Start () 
+	{
+		Spawn ();
+	}
+
+	void Spawn () 
+	{
+		for (int i = 0; i <= numObjects; i++) 
+		{
+			Vector3 spawnLoc = new Vector3 (Random.Range (-xRange, xRange), Random.Range (-yRange, yRange), 0);
+			int objectPick = Random.Range (0, objects.Length);
+			Instantiate (objects [objectPick], spawnLoc, Random.rotation);
+		}
+	}
+}
